b9d53207b4d8e5803c75ee45f60eed26773ba893
Devesh Dipak Jadhav <dj64360@hmai.com>
2024-03-30 17:19:18 +0530

    initial commit

---
+++ code1.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class code1{
+
+    static int facto(int a){
+            int factorial=1;
+            for(int i=a;i>=1;i--){
+                factorial=factorial*(i);
+            }
+            return factorial;
+    }
+    public static void main(String [] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the value of n:");
+        int n=sc.nextInt();
+        int a=facto(n);
+        System.out.println("the factorial of "+n+" is :"+a);
+
+    }
+}
